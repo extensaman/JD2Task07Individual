c270bc533fbd4e41ebf3c711fe448d6b8d5b20a6
extensaman <verus.wedding@gmail.com>
2022-04-08 11:43:08 +0300

    Initial commit

---
+++ src/main/java/by/academy/it/task07individual/App.java
@@ -0,0 +1,9 @@
+package by.academy.it.task07individual;
+
+public class App
+{
+    public static void main( String[] args )
+    {
+
+    }
+}
---
+++ src/test/java/by/academy/it/task07individual/AppTest.java
@@ -0,0 +1,19 @@
+package by.academy.it.task07individual;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest extends Assertions
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
